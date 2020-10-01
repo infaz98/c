186d8e3ad34e02dafb31e1b014cb91a7cf80a45f
Infaz <infaz.rumy@outlook.com>
2020-10-01 17:14:55 +0530

    practical directory added

---
+++ operators in c language/practical/QuestionOne.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <math.h>
+
+
+//volume of a sphere
+void main(){
+
+    float radius;
+    float volume;
+
+    printf("Enter the radius: ");
+    scanf("%f", &radius);
+
+    volume = (radius*radius*radius)*M_PI*4/3;
+
+    printf("Volume of the sphere is : %.2f\n", (volume));
+
+}
